e7f1f26b4e9c9489ccadeeb1562a68e329d09324
Anton <a6672634@yandex.ru>
2018-11-12 21:28:50 +0300

    Work2

--- src/org/itstep/qa/runner/RunnerMassiv.java
+++ src/org/itstep/qa/runner/RunnerMassiv.java
@@ -1,20 +1,30 @@
 package org.itstep.qa.runner;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class RunnerMassiv {
     public static void main(String[] args) {
         // Организовать ввод с клавиатуры массива элементов заданного
-        // типа (целых чисел).
+        // типа (целых чи1сел).
         // Проверку корректности ввода реализовать через обработку исключений.
         // (Можно отслеживать InputMismatchException).
         //В случае если пользователь ввел некорректное значение,
         // заполнять соответствующую ячейку массива значением по умолчанию
-        System.out.println("введите целые числа для массива");
-        Scanner scanner = new Scanner(System.in);
-        int nums[] ;
+                  Scanner sc = new Scanner(System.in);
+            int[] array = new int[10];
+            System.out.println("Введите любое значение");
+            for (int i = 0; i < array.length; i++) {
+                try {
+                    array[i] = sc.nextInt();
+                } catch (InputMismatchException ex) {
 
-
-
+                    array[i] = 0;
+                    sc.next();
+                }
+            }
+            for (int i = 0; i < array.length; i++)
+                System.out.print(array[i] + " ");
+        }
     }
-}
+
